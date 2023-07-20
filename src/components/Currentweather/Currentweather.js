8228bae499443c91b859a086ad58eb7b56e5b1eb
Varun <varunnm68@gmail.com>
2023-07-21 00:13:20 +0530

    new commit

--- src/components/Currentweather/Currentweather.js
+++ src/components/Currentweather/Currentweather.js
@@ -6,7 +6,7 @@ const Currentweather = ({ currentWeather }) => {
   const { name, icon, description, temp, date} = currentWeather
   
   return (
-    <div>
+    <>
       <div className='current-container'>
         <div className='top-details'>
           <div className='details-container'>
@@ -19,17 +19,24 @@ const Currentweather = ({ currentWeather }) => {
         <hr/>
         <div className='bot-details'>
           <h5 className='bot-detail'>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M4.998 8H19V20H4.998z"></path><path d="M2.999,6v2v12c0,1.103,0.897,2,2,2h14c1.103,0,2-0.897,2-2V8V6c0-1.103-0.897-2-2-2h-2V2h-2v2h-6V2h-2v2h-2 C3.896,4,2.999,4.897,2.999,6z M19.001,20H4.999V8h14L19.001,20z"></path></svg>
+            <img src='https://cdn-icons-png.flaticon.com/128/3239/3239948.png' className='calender'/>
             <span className='details'>{date}</span>
           </h5>
           <h5 className='bot-detail'>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 12 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
+            <img src='https://cdn-icons-png.flaticon.com/128/3082/3082383.png' className='location-icon' />
             <span className='details'>{name}</span>
           </h5>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
 export default Currentweather
+
+{/* <h5 className='bot-detail'>
+            <img src='https://cdn-icons-png.flaticon.com/128/3239/3239948.png' className='calender'/>
+          </h5>
+          <h5 className='bot-detail'>
+            <img src='https://cdn-icons-png.flaticon.com/128/5338/5338588.png' className='location-icon' />
+          </h5> */}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,11 +34,11 @@ const Navbar = ({onCityChange,getUserCoordinates}) => {
                 onKeyDown={handleKey}
             />
             <button className='search-button' onClick={handleClick}>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"></path></svg>
+                <img src='https://cdn-icons-png.flaticon.com/128/2811/2811806.png' className='search-icon'/>
             </button>
         </div>
         <div className='current-location' onClick={getUserCoordinates}>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="4"></circle><path d="M13,4.069V2h-2v2.069C7.389,4.522,4.523,7.389,4.069,11H2v2h2.069c0.454,3.611,3.319,6.478,6.931,6.931V22h2v-2.069 c3.611-0.453,6.478-3.319,6.931-6.931H22v-2h-2.069C19.478,7.389,16.611,4.522,13,4.069z M12,18c-3.309,0-6-2.691-6-6s2.691-6,6-6 s6,2.691,6,6S15.309,18,12,18z"></path></svg>
+            <img src='https://cdn-icons-png.flaticon.com/128/1549/1549624.png' className='gps-icon'/>
             <span className='c-l'>Current location</span>
         </div>
     </nav>
